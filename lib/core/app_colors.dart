18a483f67df3ec575dd9739967adcc77d2f6ef28
Mohey Aghiela (2201805518) <mohey.mahmoud21@gmail.com>
2025-02-25 12:55:02 +0200

    some new changes + new directories

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFF192845);
+  static const Color secondaryColor = Color(0xFFDEE4EE);
+  static const Color backgroundColor = Color(0xFFF6F6F6);
+  static const Color accentColor = Color(0xFFF1E4D1);
+  static const Color textColor = Color(0xFF303030);
+}
