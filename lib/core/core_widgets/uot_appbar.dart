8f1642c850093a6196215b6fc451eb9b4ca8b718
wasn abdulgader <wasnwasn02@gmail.com>
2025-03-08 21:43:31 +0200

    Notifications Screen
    Notifications screen / NotificationsWidget

--- lib/core/core_widgets/uot_appbar.dart
+++ lib/core/core_widgets/uot_appbar.dart
@@ -1,43 +1,53 @@
 import 'package:flutter/material.dart';
-          import 'package:flutter_svg/flutter_svg.dart';
-          import 'package:uot_transport/core/app_colors.dart';
-          import 'package:uot_transport/core/app_icons.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:uot_transport/core/app_colors.dart';
+import 'package:uot_transport/core/app_icons.dart';
+import 'package:uot_transport/core/notifications_screen.dart';
 
-          class UotAppbar extends StatelessWidget implements PreferredSizeWidget {
-            const UotAppbar({super.key});
+class UotAppbar extends StatelessWidget implements PreferredSizeWidget {
+  const UotAppbar({super.key});
 
-            @override
-            Widget build(BuildContext context) {
-              return Directionality(
-                textDirection: TextDirection.rtl,
-                child: AppBar(
-                  leading: Padding(
-                    padding: const EdgeInsets.only(right: 10), // Adjust the padding value as needed
-                    child: Image.asset(AppIcons.logoPath, width: 80, height: 80),
-                  ),
-                  backgroundColor: AppColors.backgroundColor,
-                  elevation: 0,
-                  actions: [
-                    IconButton(
-                      icon: SvgPicture.asset(AppIcons.outline_NotificationsAppPath, width: 20, height: 22, color: AppColors.primaryColor),
-                      onPressed: () {},
-                    ),
-                    IconButton(
-                      icon: SvgPicture.asset(AppIcons.outline_SearchAppPath, width: 24, height: 24, color: AppColors.primaryColor),
-                      onPressed: () {},
-                    ),
-                  ],
-                  bottom: PreferredSize(
-                    preferredSize: const Size.fromHeight(1.0),
-                    child: Container(
-                      color: Color(0xffCED2D8),
-                      height: 1.0,
-                    ),
-                  ),
-                ),
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.only(
+              right: 10), // Adjust the padding value as needed
+          child: Image.asset(AppIcons.logoPath, width: 80, height: 80),
+        ),
+        backgroundColor: AppColors.backgroundColor,
+        elevation: 0,
+        actions: [
+          IconButton(
+            icon: SvgPicture.asset(AppIcons.outline_NotificationsAppPath,
+                width: 20, height: 22, color: AppColors.primaryColor),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => const Notifications()),
               );
-            }
+            },
+          ),
+          IconButton(
+            icon: SvgPicture.asset(AppIcons.outline_SearchAppPath,
+                width: 24, height: 24, color: AppColors.primaryColor),
+            onPressed: () {},
+          ),
+        ],
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(1.0),
+          child: Container(
+            color: Color(0xffCED2D8),
+            height: 1.0,
+          ),
+        ),
+      ),
+    );
+  }
 
-            @override
-            Size get preferredSize => const Size.fromHeight(56);
-          }
+  @override
+  Size get preferredSize => const Size.fromHeight(56);
+}
---
+++ lib/core/notifications_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:uot_transport/auth_feature/view/widgets/app_text.dart';
+import 'package:uot_transport/core/app_colors.dart';
+import 'package:uot_transport/core/core_widgets/notifications_widget.dart';
+import 'package:uot_transport/core/core_widgets/uot_appbar.dart';
+
+class Notifications extends StatelessWidget {
+  const Notifications({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: UotAppbar(),
+        backgroundColor: AppColors.backgroundColor,
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 28, right: 28),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AppText(
+                    lbl: 'الإشعارات',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            NotificationsWidget(
+              notificationText: 'وصلت الحافلة 1 لمحطة قصر بن غشير',
+            ),
+            NotificationsWidget(
+              notificationText: 'تأخرت الرحلة 10 دقائق عن الموعد المتوقع',
+            ),
+            NotificationsWidget(
+              notificationText: 'وصلت الحافلة 1 لمحطة قصر بن غشير',
+            ),
+            NotificationsWidget(
+              notificationText: 'تأخرت الرحلة 10 دقائق عن الموعد المتوقع',
+            ),
+            NotificationsWidget(
+              notificationText: 'وصلت الحافلة 1 لمحطة قصر بن غشير',
+            ),
+            NotificationsWidget(
+              notificationText: 'تأخرت الرحلة 10 دقائق عن الموعد المتوقع',
+            ),
+            NotificationsWidget(
+              notificationText: 'وصلت الحافلة 1 لمحطة قصر بن غشير',
+            ),
+            NotificationsWidget(
+              notificationText: 'تأخرت الرحلة 10 دقائق عن الموعد المتوقع',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
